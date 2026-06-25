5a706d4dbda7c09445e6845c442f2f8194230568
unknown <dexperienca@gmail.com>
2026-06-25 20:10:51 -0300

    fix: adiciona ícones PWA e corrige .gitignore para não ignorar static/icons

--- static/sw.js
+++ static/sw.js
@@ -4,6 +4,8 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         '/static/manifest.json',
+        '/static/icons/icon-192.png',
+        '/static/icons/icon-512.png',
         'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
         'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css'
       ]);
